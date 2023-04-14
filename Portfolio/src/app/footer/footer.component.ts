04702176f5aead250c0b9186fb5b7fef9a758a4e
Bruno Pizzi <b.pizzipe@gmail.com>
2023-04-14 17:34:21 -0300

    Add formspree service to receive msg on my mail

--- Portfolio/src/app/footer/footer.component.ts
+++ Portfolio/src/app/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MessageService } from '../service/message.service';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -12,12 +14,42 @@ export class FooterComponent implements OnInit {
   name: any;
   message:any
 
-  constructor() { }
+  constructor(private sendData:MessageService) { }
 
   ngOnInit(): void {
   }
   
-
+  sendForm(body:any){
+    console.log(body);
+    this.sendData.SendToFormspree(body).subscribe(
+      response => {
+        console.log('Respuesta del servidor:', response);
+        // Maneja la respuesta del servidor aquí
+        
+        this.closeModal()
+      },
+      error => {
+        console.error('Error en la solicitud:', error);
+        // Maneja el error aquí
+      }
+    );
+  }
+  
+  closeModal() {
+    const modal = document.getElementById!('exampleModal');
+    modal!.classList.remove('show');
+    modal!.setAttribute('aria-hidden', 'true');
+    modal!.setAttribute('style', 'display: none');
+  
+    const modalBackdrops = document.getElementsByClassName!('modal-backdrop');
+    for (let i = 0; i < modalBackdrops.length; i++) {
+      modalBackdrops[i].parentNode!.removeChild(modalBackdrops[i]);
+    }
+  
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('modal-open');
+    body.removeAttribute('style');
+  }
   
 }
 
---
+++ Portfolio/src/app/service/message.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor(private http: HttpClient) { }
+
+SendToFormspree(body:any): Observable<any> {
+  return this.http.post("https://formspree.io/f/xayzlvjb",body)
+}
+}
